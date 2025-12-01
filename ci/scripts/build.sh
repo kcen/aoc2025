a776f48e693b40bf611be5132569d76c1c186127
Casey <kcen@users.noreply.github.com>
2025-12-01 03:05:03 -0800

    force native arch building

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nimble --out:dist/kcen-aoc --threads:on --opt:speed -d:release -d:danger --mm:orc c aoc.nim
+nimble --out:dist/kcen-aoc -d:danger --mm:orc -d:lto --passC:-march=native c aoc.nim

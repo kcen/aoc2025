b243f4d09d1e79a01a2e836dd05b9e2931f1202f
Casey <kcen@users.noreply.github.com>
2025-12-03 11:29:55 -0800

    dont use stack, manually keep track of position

--- aoc/day_03.nim
+++ aoc/day_03.nim
@@ -2,15 +2,24 @@ import aoc_utils
 import std/parseutils
 
 proc max_joltage(battery_chars: seq[char], on_size: int): int =
-  var removals = battery_chars.len - on_size
-  var stack: seq[char] = @[]
+  var joltage: seq[char] = @[]
+  var min_index = 0
 
-  for digit in battery_chars:
-    while removals > 0 and stack.len > 0 and stack[^1].ord < digit.ord:
-      discard stack.pop()
-      removals -= 1
-    stack.add digit
-  discard stack[0..(on_size-1)].parseInt(result)
+  let size_max = battery_chars.len - 1
+  let size_index = on_size - 1
+  let start_index = size_max - size_index
+
+  for digit_pos in 0..size_index:
+    var largest = '0'
+    var index_pos = 0
+    let max_index = (start_index + digit_pos)
+    for i in min_index..max_index:
+      if battery_chars[i].ord > largest.ord:
+        largest = battery_chars[i]
+        index_pos = i
+    min_index = index_pos + 1
+    joltage.add(largest)
+  discard joltage.parseInt(result)
 
 proc day_03*(): Solution =
   var part1 = 0

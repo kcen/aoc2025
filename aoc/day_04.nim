be69b64232134680244e17a7fb12415b0d3a16d8
Casey <kcen@users.noreply.github.com>
2025-12-03 23:16:24 -0800

    keep track of neighbors count and manage them explicitly

--- aoc/day_04.nim
+++ aoc/day_04.nim
@@ -3,6 +3,12 @@ import aoc_utils
 
 proc day_04*(): Solution =
   var grid = getInput().parseSparseGrid('.')
+  var counts = initTable[Coord, int]()
+
+  # keep track of how many neighbors
+  for pos, _ in grid.data.pairs:
+    counts[pos] = grid.neighbors(pos, diagonals = true).len
+
   var first_accessible = 0
   var total = 0
   var first_look = true
@@ -10,8 +16,8 @@ proc day_04*(): Solution =
   while true:
     var accessible: seq[Coord] = @[]
 
-    for pos, value in grid.data.pairs:
-      if grid.neighbors(pos, diagonals = true).len < 4:
+    for pos, _ in grid.data.pairs:
+      if counts[pos] < 4:
         accessible.add(pos)
 
     if accessible.len == 0: break # quittin' time
@@ -23,6 +29,10 @@ proc day_04*(): Solution =
     total += accessible.len
 
     for pos in accessible:
+      for neighbor in grid.neighbors(pos, diagonals = true):
+        if counts.hasKey(neighbor):
+          counts[neighbor] -= 1
       grid.data.del(pos)
+      counts.del(pos)
 
   Solution(part_one: $first_accessible, part_two: $total)
